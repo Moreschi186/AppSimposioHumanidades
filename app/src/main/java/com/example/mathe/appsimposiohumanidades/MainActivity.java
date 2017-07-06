083171fc5f21dcb87d605e3cc4b76b0d0102bb1f
matheusrenan <matheusrenan@gmail.com>
2017-07-06 19:09:16 -0300

    2.0

--- app/src/main/java/com/example/mathe/appsimposiohumanidades/MainActivity.java
+++ app/src/main/java/com/example/mathe/appsimposiohumanidades/MainActivity.java
@@ -49,38 +49,15 @@ public class MainActivity extends Activity {
     public void verificaEvento() {
         TextView ocorrendoAgora = (TextView) findViewById(R.id.label_evento);
         SimpleDateFormat dateFormat_hora = new SimpleDateFormat("HH:mm");
-        SimpleDateFormat dateFormat_dia = new SimpleDateFormat("dd:MM");
+        SimpleDateFormat dateFormat_dia = new SimpleDateFormat("dd/MM/yyyy");
         Date data = new Date();
         Calendar cal = Calendar.getInstance();
         cal.setTime(data);
         Date data_atual = cal.getTime();
         String hora_atual = dateFormat_hora.format(data_atual);
         String dia_atual = dateFormat_dia.format(data_atual);
-        String horacerta = hora_atual;
-        horacerta = horacerta.replace(":", "");
-        String diacerto = dia_atual;
-        diacerto = dia_atual.replace(":", "");
-        int dia = Integer.parseInt(diacerto);
-        int hora = Integer.parseInt(horacerta);
-        System.out.println("Dia:" + dia + " " + "Hora: " + hora);
-         if (dia == 2808) {
-            if (hora >= 1830 && hora < 1930) {
-                ocorrendoAgora.setText("Solenidade de Abertura");
-            }
-            else if (hora >= 1930 && hora < 2200) {
-                ocorrendoAgora.setText("Educação no Brasil desafios para a formação de sujeitos críticos");
-            }
-            else {
-                ocorrendoAgora.setText("Não está ocorrendo nenhum evento neste momento");
-            }
-        }
-        else if (dia == 2908) { // Popular com eventos do dia 29
-
-        }
-        else if (dia == 3008) { // Popular com eventos do dia 30
-
-        }
-
+        System.out.println("Dia:" + dia_atual + " " + "Hora: " + hora_atual);
+        
     }
 
 }
