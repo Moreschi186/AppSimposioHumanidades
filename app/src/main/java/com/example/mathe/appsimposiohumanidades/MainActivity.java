39eefa272461c353673cdaa5ee43fac5330005d2
matheusrenan <matheusrenan@gmail.com>
2017-07-07 15:31:19 -0300

    Adicionado classe eventos.

--- app/src/main/java/com/example/mathe/appsimposiohumanidades/MainActivity.java
+++ app/src/main/java/com/example/mathe/appsimposiohumanidades/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mathe.appsimposiohumanidades;
 
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,16 +24,17 @@ public class MainActivity extends Activity {
     private Timer timerAtual = new Timer();
     private TimerTask task;
     private final Handler handler = new Handler();
-
+    TextView ocorrendoAgora;
+    Eventos evento = new Eventos();
 
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ocorrendoAgora = (TextView) findViewById(R.id.label_evento);
         ativaTimer();
     }
 
-    public void ativaTimer() {
+    private void ativaTimer(){
         task = new TimerTask() {
             public void run() {
                 handler.post(new Runnable() {
@@ -40,24 +42,15 @@ public class MainActivity extends Activity {
                         verificaEvento();
                     }
                 });
-            }
-        };
-
-        timerAtual.schedule(task, 100 * 60, 10000);
+            }};
+        timerAtual.schedule(task, 1, 1000);
     }
 
-    public void verificaEvento() {
-        TextView ocorrendoAgora = (TextView) findViewById(R.id.label_evento);
-        SimpleDateFormat dateFormat_hora = new SimpleDateFormat("HH:mm");
-        SimpleDateFormat dateFormat_dia = new SimpleDateFormat("dd/MM/yyyy");
-        Date data = new Date();
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(data);
-        Date data_atual = cal.getTime();
-        String hora_atual = dateFormat_hora.format(data_atual);
-        String dia_atual = dateFormat_dia.format(data_atual);
-        System.out.println("Dia:" + dia_atual + " " + "Hora: " + hora_atual);
-        
+        public void verificaEvento() { // Verifica o horario do celular e mostra o evento que está ocorrendo no momento.
+            int eventoAtual;
+            Timestamp horaAtual = new Timestamp(System.currentTimeMillis());
+            eventoAtual = evento.retornaEvento(horaAtual);
+            System.out.println("\nHorário: " + horaAtual + " " + "Evento atual: " + eventoAtual);
+            ocorrendoAgora.setText("Funfando");
     }
-
 }
