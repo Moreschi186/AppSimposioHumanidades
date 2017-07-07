20130f25955b106bce9ee7b6d636ff2f1535348a
matheusrenan <matheusrenan@gmail.com>
2017-07-07 16:53:05 -0300

    07/07/2016 * Função de verificação de evento funcionando.    - Verifica a hora do celular e já exibe em um JtextArea qual o evento está ocorrendo no momento. Utiliza TimeStamp.

--- app/src/main/java/com/example/mathe/appsimposiohumanidades/MainActivity.java
+++ app/src/main/java/com/example/mathe/appsimposiohumanidades/MainActivity.java
@@ -52,7 +52,40 @@ public class MainActivity extends Activity {
         Timestamp horaAtual = new Timestamp(System.currentTimeMillis());
         eventoAtual = evento.retornaEvento(horaAtual);
         System.out.println("\nHorário: " + horaAtual + " " + "Evento atual: " + eventoAtual);
-        ocorrendoAgora.setText("Funfando");
+        switch (eventoAtual){
+            case 0:
+                ocorrendoAgora.setText("O evento acabou. Obrigado pela sua participação. Não esqueça de avaliar o quê você achou do evento na aba Avalie o Evento");
+            case 1:
+                ocorrendoAgora.setText("O evento ainda não iniciou. Acesse a aba Programação para ver a programação completa do evento");
+            case 2:
+                ocorrendoAgora.setText("Solenidade de abertura - Local: Auditório");
+            case 3:
+                ocorrendoAgora.setText("Educação no Brasil desafios para a formação de sujeitos críticos");
+            case 4:
+                ocorrendoAgora.setText("O primeiro dia do evento acabou. Fique atento a programação do segundo dia na aba Programação");
+            case 5:
+                ocorrendoAgora.setText("EVENTOS MANHÃ SEGUNDO DIA");
+            case 6:
+                ocorrendoAgora.setText("Atividades Culturais");
+            case 7:
+                ocorrendoAgora.setText("EVENTOS TARDE SEGUNDO DIA");
+            case 8:
+                ocorrendoAgora.setText("Nenhuma palestra acontecendo no momento. Acesse a aba Programação e confira a programação completa do evento");
+            case 9:
+                ocorrendoAgora.setText("Escola para quê(m)?");
+            case 10:
+                ocorrendoAgora.setText("O segundo dia do evento acabou. Fique atento a programação do segundo dia na aba Programação");
+            case 11:
+                ocorrendoAgora.setText("EVENTOS MANHÃ TERCEIRO DIA");
+            case 12:
+                ocorrendoAgora.setText("Atividades Culturais");
+            case 13:
+                ocorrendoAgora.setText("EVENTOS TARDE TERCEIRO DIA");
+            case 14:
+                ocorrendoAgora.setText("Nenhuma palestra acontecendo no momento. Acesse a aba Programação e confira a programação completa do evento");
+            case 15:
+                ocorrendoAgora.setText("Educação e diversidade");
+        }
     }
 
     public void chamaMenu(View view){
